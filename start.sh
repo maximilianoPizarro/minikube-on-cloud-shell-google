a936bca87c0fa46b6f908422dd6cb712be9e0add
Maximiliano Pizarro <maximiliano.pizarro.5@gmail.com>
2022-09-14 17:45:23 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ kubectl create deployment httpd --image=httpd --port=80
 
 echo "waiting the pod with status Running"
 echo "---------------------------"
-for podname in $(kubectl get endpoints httpd -o json | jq -r 'select(.subsets != null) | .subsets[].addresses[].targetRef.name')
+for podname in $(kubectl get pods $podname  -o json | jq -r ' select(.status.conditions[].type == "Running") | .status.conditions[].type ')
 do
 kubectl get pods $podname  -o json | jq -r ' select(.status.conditions[].type == "Running") | .status.conditions[].type ' | grep -x Running
 done
